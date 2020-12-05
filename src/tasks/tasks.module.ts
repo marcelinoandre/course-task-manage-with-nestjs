e679c356a344d97f5a7d488f39fc963bf184a696
Andre Marcelino <marcelinoandre@gmail.com>
2020-12-04 23:43:41 -0300

    feat: configuration jwt finish

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [TypeOrmModule.forFeature([TaskRepository]), AuthModule],
   providers: [TasksService],
   controllers: [TasksController],
 })
