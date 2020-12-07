1d7665d26013bc91386edf142fdb9f1d58f1b6bb
Andre Marcelino <marcelinoandre@gmail.com>
2020-12-07 20:19:31 -0300

    feat: add configs

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as config from 'config';
 
 async function bootstrap() {
-  const logger = new Logger('bootstrap');
-  const port = 3333;
-  const app = await NestFactory.create(AppModule);
-  await app.listen(port);
-  logger.log(`Application listerning on port ${port}`);
+                             const serverConfig = config.get('server');
+                             const logger = new Logger('bootstrap');
+                             const port =
+                               process.env.PORTx || serverConfig['port'];
+                             const app = await NestFactory.create(AppModule);
+                             await app.listen(port);
 
-  console.log(process.env.NODE_ENV);
-}
+                             logger.log(
+                               `Application listerning on port ${port}`,
+                             );
+
+
+                             console.log(process.env.PORTx);
+                           }
 bootstrap();
